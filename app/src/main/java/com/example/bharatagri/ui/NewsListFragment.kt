0fde1d51cf54a14b5214d063b133719d8abc594b
rajnikant-pandey <rajnikant.pandey@cricbuzz.com>
2022-02-07 20:17:58 +0530

    Code Formated

--- app/src/main/java/com/example/bharatagri/ui/NewsListFragment.kt
+++ app/src/main/java/com/example/bharatagri/ui/NewsListFragment.kt
@@ -3,28 +3,27 @@ package com.example.bharatagri.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.bharatagri.R
 import com.example.bharatagri.adapter.MovieListAdapter
-import com.example.bharatagri.viewmodels.NewsListViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.appcompat.app.AppCompatActivity
 import com.example.bharatagri.databinding.FragmentNewsListBinding
 import com.example.bharatagri.repository.NewsArticle
 import com.example.bharatagri.repository.NewsArticleResponse
 import com.example.bharatagri.utils.*
+import com.example.bharatagri.viewmodels.NewsListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NewsListFragment : ListItemFragment<MovieListAdapter, FragmentNewsListBinding>(), RecycleViewItemViewClickListener<NewsArticle> {
+class NewsListFragment : ListItemFragment<MovieListAdapter, FragmentNewsListBinding>(),
+    RecycleViewItemViewClickListener<NewsArticle> {
     override val fragmentLayoutResId: Int
         get() = R.layout.fragment_news_list
     private val newsListViewModel: NewsListViewModel by viewModels()
     lateinit var navController: NavController
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
@@ -32,16 +31,18 @@ class NewsListFragment : ListItemFragment<MovieListAdapter, FragmentNewsListBind
         (activity as AppCompatActivity?)!!.supportActionBar!!.title = getString(R.string.news)
         adapter.setOnClickListener(this)
     }
+
     override fun loadData() {
         newsListViewModel.getNewsList(1)
     }
+
     override fun receivedResponse(item: Any?) {
         item?.let { response ->
             when (response) {
                 is NewsArticleResponse -> {
                     response.articles?.let { it1 ->
-                        if(it1.size>0) {
-                            paginationEnabled=true
+                        if (it1.size > 0) {
+                            paginationEnabled = true
                             adapter.setItems(it1)
                         }
                     }
@@ -51,9 +52,10 @@ class NewsListFragment : ListItemFragment<MovieListAdapter, FragmentNewsListBind
             }
         }
     }
+
     override fun onClick(item: NewsArticle) {
         val bundle = Bundle()
-        bundle.putString(NEWS_IMAGE,item.urlToImage)
+        bundle.putString(NEWS_IMAGE, item.urlToImage)
         bundle.putString(NEWS_TITLE, item.title)
         bundle.putString(DESCRIPTION, item.description)
         bundle.putString(NEWS_CONTENT, item.content)
@@ -61,14 +63,15 @@ class NewsListFragment : ListItemFragment<MovieListAdapter, FragmentNewsListBind
         bundle.putString(PUBLISHED_DATE, item.publishedAt)
         navController.navigate(R.id.action_newsListFragment_to_newsDetailsFragment, bundle)
     }
-    override fun loadPage(currentPage:Int) {
-            addLoadingItem(adapter.itemCount, adapter.getItems())
-             newsListViewModel.getNewsList(currentPage)
+
+    override fun loadPage(currentPage: Int) {
+        addLoadingItem(adapter.itemCount, adapter.getItems())
+        newsListViewModel.getNewsList(currentPage)
     }
 
     override fun removeLoadingItem() {
-        Log.d("xdfgfdxdxkj", (adapter.itemCount-1).toString())
-        removeLoadingItem(adapter.itemCount-1,adapter.getItems())
+        Log.d("xdfgfdxdxkj", (adapter.itemCount - 1).toString())
+        removeLoadingItem(adapter.itemCount - 1, adapter.getItems())
     }
 
 }
